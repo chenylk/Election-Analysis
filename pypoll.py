52d2643a3f4221ec09667527b66f204ab16b3b5c
Data Boot Camp <chenylk@outlook.com>
2020-07-28 23:12:30 -0400

    Updating Python Code

--- pypoll.py
+++ pypoll.py
@@ -12,9 +12,12 @@ with open(file_to_load) as election_data:
 
     file_reader = csv.reader(election_data)
 
+    #Read the header row
     headers = next(file_reader)
     print(headers)
    
-
+    # Print each row in the CSV file
+    for row in file_reader:
+        print(row)
 
 

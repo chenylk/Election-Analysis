6cd6169d0f44d72528f95a73aeb1cbef52185cd6
Data Boot Camp <chenylk@outlook.com>
2020-07-29 22:37:04 -0400

    updated pypoll code

--- pypoll.py
+++ pypoll.py
@@ -7,6 +7,12 @@ file_to_load = os.path.join("resources","election_results.csv")
 #Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+#initialize total votes counter
+total_votes = 0
+
+#create a list of all possible candidates 
+candidate_options = []
+
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -18,6 +24,25 @@ with open(file_to_load) as election_data:
    
     # Print each row in the CSV file
     for row in file_reader:
-        print(row)
+        
+        #Add to the total vote count
+        total_votes += 1
+        
+        #Grab the candidates name from the row
+        candidate_name = row[2]
+
+        #If the candidate is not already added to the list
+        if candidate_name not in candidate_options:
+
+            #Add the candidates name to the list
+            candidate_options.append(candidate_name)
+
+
+print(total_votes)
+
+print(candidate_options)
+
+
+
 
 

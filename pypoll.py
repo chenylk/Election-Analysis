bc07ea6c2a3d2e8746f0eaf556175fbd996e1972
Data Boot Camp <chenylk@outlook.com>
2020-07-30 00:02:25 -0400

    updated analysis code

--- pypoll.py
+++ pypoll.py
@@ -13,6 +13,14 @@ total_votes = 0
 #create a list of all possible candidates 
 candidate_options = []
 
+candidate_votes = {}
+
+#Winning candidate and winning count tracker
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
+
+
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -31,17 +39,38 @@ with open(file_to_load) as election_data:
         #Grab the candidates name from the row
         candidate_name = row[2]
 
+
+
         #If the candidate is not already added to the list
         if candidate_name not in candidate_options:
 
             #Add the candidates name to the list
             candidate_options.append(candidate_name)
+            
+            #Add the different candidates to the dictionary as keys with 0 initial value
+            candidate_votes[candidate_name] = 0
 
+            
+        candidate_votes[candidate_name] += 1
 
-print(total_votes)
+    for candidate_name in candidate_votes:
 
-print(candidate_options)
+        votes = candidate_votes[candidate_name]
 
+        percentage_votes = float(votes)/float(total_votes) * 100
+
+        print(f'{candidate_name} recieved {percentage_votes:.1f} % of the votes')
+
+        #Determine if the votes are greater than the winning count
+        if (votes > winning_count) and (percentage_votes > winning_percentage):
+
+            #if true then set the variables
+            winning_count = votes 
+            winning_percentage = percentage_votes
+            winning_candidate = candidate_name
+
+    
+print('The winning candidate is {} with a voting percentage of {:.1f}%'.format(winning_candidate,winning_percentage))
 
 
 
